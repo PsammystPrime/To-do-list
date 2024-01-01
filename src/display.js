414e22bbac3ea88fa149bd19050e836d14ae37a0
PsammystPrime <samuelprime384@gmail.com>
2024-01-01 10:58:01 +0300

    Create teams functionality

--- src/display.js
+++ src/display.js
@@ -1,4 +1,4 @@
-import { storage, projectsStore } from './pageLoad';
+import { storage, projectsStore, teamStore } from './pageLoad';
 /******Displays new cards*///////
 
 function cards() {   
@@ -84,7 +84,7 @@ function projects() {
     const header = document.getElementById('header');
     header.textContent = '';
     header.textContent = 'My Projects';
-     article.textContent = ''
+    article.textContent = ''
 
 
     projectsStore.forEach(function(project){
@@ -112,22 +112,49 @@ function projects() {
         })
       
         article.appendChild(card)
-          console.log(project)
     })
 
 
           
  };
-// function teams() {
-//     const article = document.getElementById('article');
-//     const header = document.getElementById('header');
-//     header.textContent = '';
-//     header.textContent = 'Team';
-//     article.textContent= '';
 
-// };
+function teams() {
+    const article = document.getElementById('article');
+    const header = document.getElementById('header');
+    header.textContent = '';
+    header.textContent = 'Team';
+    article.textContent= '';
+
+    teamStore.forEach(function(project){
+  
+      const card = document.createElement('article');
+      const cardBtn = document.createElement('button');
+      const cardDetails = document.createElement('div');
+      const cardName = document.createElement('p');
+      const cardTime = document.createElement('p');
+      card.id = 'card'
+      cardDetails.id = 'details'
+      cardBtn.textContent = 'X';
+      cardName.textContent = project//'Put details here' 
+      cardTime.textContent = 'Due: 3:00 am'
+      
+      cardDetails.appendChild(cardName);
+      cardDetails.appendChild(cardTime);
+      
+      card.appendChild(cardBtn);
+      card.appendChild(cardDetails);
+      
+      
+        cardBtn.addEventListener('click', ()=>{
+          article.removeChild(card)
+        })
+      
+        article.appendChild(card)
+    })
+
+};
 
 // export {cards, toDay,searcH,inboxx,projects,teams}
-export{cards,projects}
+export{cards,projects,teams}
 
 
