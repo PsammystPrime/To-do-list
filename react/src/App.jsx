7fccac67d24e337c9202f792b0772d2c5865f5ff
PsammystPrime <samuelprime384@gmail.com>
2024-07-17 06:55:28 +0300

    create landing page

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import logo from "/logo.png";
+import pana from "/pana.jpg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <main>
+      <img className="logo" src={logo} alt="logo" />
+      <img className="pana" src={pana} alt="landing Page Image" />
+      <p>
+        Manage <br />
+        your <br /> Task with <br /> <a className="daytask">DayTask</a>
       </p>
-    </>
-  )
+      <button>Let's Start</button>
+    </main>
+  );
 }
 
-export default App
+export default App;
