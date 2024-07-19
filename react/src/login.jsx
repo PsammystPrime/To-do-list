5d2e4c4ace7c64d22b7174eb59d7818576c2d90f
PsammystPrime <samuelprime384@gmail.com>
2024-07-19 10:43:26 +0300

    update login.jsx

--- react/src/login.jsx
+++ react/src/login.jsx
@@ -4,27 +4,29 @@ import logo from "/logo.png";
 
 export function LoginPage() {
   return (
-    <>
-      <img className="logo" src={logo} alt="logo" />
+    <main>
+      <img className="login-logo" src={logo} alt="logo" />
       <h2>Welcome Back!</h2>
       <form action="" method="post">
         <label htmlFor="email">Email Address</label>
         <input type="email" name="email" id="" />
         <label htmlFor="password">Password</label>
         <input type="password" name="" id="" />
-        <p>Forgot Passsword?</p>
-        <button type="submit">Log In</button>
+        <h6>Forgot Passsword?</h6>
+        <button type="submit" className="login">
+          Log In
+        </button>
       </form>
       <section>
         <div></div>
         <p>Or continue with</p>
         <div></div>
       </section>
-      <button>Google</button>
+      <button className="google">Google</button>
       <p>
-        Don`&apos;`t have an account? <a href="">Sign Up</a>
+        Don&apos;t have an account? <a href="">Sign Up</a>
       </p>
-    </>
+    </main>
   );
 }
 
