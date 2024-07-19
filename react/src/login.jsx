39e35acc94ae0daa52601fe828c673877f3ccc11
PsammystPrime <samuelprime384@gmail.com>
2024-07-19 14:12:42 +0300

    add links to login button

--- react/src/login.jsx
+++ react/src/login.jsx
@@ -14,7 +14,7 @@ export function LoginPage() {
           <label htmlFor="password">Password</label>
           <input type="password" name="" id="" />
           <span>Forgot Passsword?</span>
-          <Link to={"/signup"} className="links">
+          <Link to={"homepage"} className="links">
             <button type="submit" className="login">
               Log In
             </button>
@@ -27,14 +27,68 @@ export function LoginPage() {
         </section>
         <button className="google">Google</button>
         <p>
-          Don&apos;t have an account? <a href="">Sign Up</a>
+          Don&apos;t have an account?{" "}
+          <Link to={"signup"}>
+            <a>Sign Up</a>
+          </Link>
         </p>
       </div>
     </main>
   );
 }
 export function Signup() {
-  return <p>hello</p>;
+  return (
+    <main>
+      <div className="login-container">
+        <img className="login-logo" src={logo} alt="logo" />
+        <h2>Create your account</h2>
+        <form action="" method="post">
+          <label htmlFor="name">Full Name</label>
+          <input
+            type="text"
+            name="name"
+            id=""
+            placeholder="Psammyst Prime "
+            required
+          />
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="email"
+            name="email"
+            id=""
+            placeholder="123@gmail.com"
+            required
+          />
+          <label htmlFor="password" required>
+            Password
+          </label>
+          <input type="password" name="" id="" />{" "}
+          <label htmlFor="password" required>
+            Confirm Password
+          </label>
+          <input type="password" name="" id="" />
+          <span>Forgot Passsword?</span>
+          <Link to={"/signup"} className="links">
+            <button type="submit" className="login">
+              Sign up
+            </button>
+          </Link>
+        </form>
+        <section>
+          <hr />
+          <p>Or continue with</p>
+          <hr />
+        </section>
+        <button className="google">Google</button>
+        <p>
+          Don&apos;t have an account?{" "}
+          <Link to={"/login"}>
+            <a href="">Log In</a>
+          </Link>
+        </p>
+      </div>
+    </main>
+  );
 }
 export default function Login() {
   return <LoginPage></LoginPage>;
