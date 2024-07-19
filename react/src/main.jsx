217b09168fec1cd037057857a41b78f7431dd6fe
PsammystPrime <samuelprime384@gmail.com>
2024-07-19 14:32:25 +0300

    create homepage

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -4,11 +4,13 @@ import App from "./App.jsx";
 import "./index.css";
 import Login, { Signup } from "./login.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./home.jsx";
 
 const route = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "login", element: <Login /> },
   { path: "signup", element: <Signup /> },
+  { path: "homepage", element: <Home /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
