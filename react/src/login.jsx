0e2c0f1a5ff10ea9c1128f0d1ae01c79c3276867
PsammystPrime <samuelprime384@gmail.com>
2024-07-19 10:09:41 +0300

    create login page

--- react/src/login.jsx
+++ react/src/login.jsx
@@ -1,5 +1,33 @@
 // import { Link } from "react-router-dom";
 
+import logo from "/logo.png";
+
+export function LoginPage() {
+  return (
+    <>
+      <img className="logo" src={logo} alt="logo" />
+      <h2>Welcome Back!</h2>
+      <form action="" method="post">
+        <label htmlFor="email">Email Address</label>
+        <input type="email" name="email" id="" />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="" id="" />
+        <p>Forgot Passsword?</p>
+        <button type="submit">Log In</button>
+      </form>
+      <section>
+        <div></div>
+        <p>Or continue with</p>
+        <div></div>
+      </section>
+      <button>Google</button>
+      <p>
+        Don`&apos;`t have an account? <a href="">Sign Up</a>
+      </p>
+    </>
+  );
+}
+
 export default function Login() {
-  return <div>sign up page coming soon</div>;
+  return <LoginPage></LoginPage>;
 }
