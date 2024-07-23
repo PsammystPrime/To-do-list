140d6c5cd5b72ff9a936c1c0fa1f64ebe56854d0
PsammystPrime <samuelprime384@gmail.com>
2024-07-23 18:05:28 +0300

    create objects array

--- react/src/home.jsx
+++ react/src/home.jsx
@@ -1,6 +1,26 @@
 import profile from "/profile1.png";
 
 export default function Home() {
+  // create tasks objects
+  function Task(key, title, status) {
+    (this.title = title), (this.status = status), (this.key = key);
+  }
+  const tsk1 = new Task(0, "design", "complete");
+  const tsk2 = new Task(1, "write", "incomplete");
+  const tsk3 = new Task(2, "docs", "incomplete");
+  // create array and store the tasks
+  const taskArray = [];
+  taskArray.push(tsk1, tsk2, tsk3);
+  console.log(taskArray);
+
+  const listItems = taskArray.map((task) => (
+    <li key={task.key}>
+      <h3>{task.title}</h3>
+      <span>Team Members</span>
+      <p className="status">{task.status}</p>
+    </li>
+  ));
+
   return (
     <main>
       <div className="home-container">
@@ -11,14 +31,21 @@ export default function Home() {
           </div>
           <img src={profile} alt="profile picture" className="profile-pic" />
         </div>
+        {/* Search bar */}
         <div className="search">
           <input type="search" name="" id="search" />
           <button>S</button>
         </div>
-        <div className="complete">
-          <h4>Completed Tasks</h4>
-          <button>See all</button>
+        {/* Completed Tasks */}
+        <div id="complete">
+          <div className="complete">
+            <h4>Completed Tasks</h4>
+            <button>See all</button>
+          </div>
+          <ul>{listItems}</ul>
         </div>
+
+        {/* Ongoing Tasks */}
         <div className="ongoing">
           <h4>Ongoing Projects</h4>
           <button>See all</button>
@@ -27,3 +54,14 @@ export default function Home() {
     </main>
   );
 }
+
+// const listItems = taskArray.map((person) => (
+//   <li key={person.key}>
+//     <p>
+//       <b>{person.title}:</b>
+//       {" " + person.status + " "}
+//       known for {person.accomplishment}
+//     </p>
+//   </li>
+// ));
+// return <ul>{listItems}</ul>;
