65915d0cea0894c1114389aae1cf69a893c1c8bc
PsammystPrime <samuelprime384@gmail.com>
2024-07-23 09:59:44 +0300

    add search bar

--- react/src/home.jsx
+++ react/src/home.jsx
@@ -1,3 +1,29 @@
+import profile from "/profile1.png";
+
 export default function Home() {
-  return <p>homepage Coming Next</p>;
+  return (
+    <main>
+      <div className="home-container">
+        <div className="profile">
+          <div className="greetings">
+            <span>Welcome Back</span>
+            <h2>Psammyst Prime</h2>
+          </div>
+          <img src={profile} alt="profile picture" className="profile-pic" />
+        </div>
+        <div className="search">
+          <input type="search" name="" id="search" />
+          <button>S</button>
+        </div>
+        <div className="complete">
+          <h4>Completed Tasks</h4>
+          <button>See all</button>
+        </div>
+        <div className="ongoing">
+          <h4>Ongoing Projects</h4>
+          <button>See all</button>
+        </div>
+      </div>
+    </main>
+  );
 }
